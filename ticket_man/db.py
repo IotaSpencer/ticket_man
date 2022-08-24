5f422fdbcf54c6e755a648d5bf660d384c61bf20
IotaSpencer <me@iotaspencer.me>
2022-08-23 23:47:27 -0400

    feat(): change db to mysql and remove sqlalchemy_aio references

--- ticket_man/db.py
+++ ticket_man/db.py
@@ -1,15 +1,20 @@
-from sqlalchemy_aio import ASYNCIO_STRATEGY
 from os.path import expanduser
+import asyncio
 
-from sqlalchemy import (create_engine)
-from sqlalchemy_aio import ASYNCIO_STRATEGY
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.future import select
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import sessionmaker
 
 from ticket_man.config import Configs
 from ticket_man.tables import *
 
 async def db():
-    engine = create_engine(
+    engine = create_async_engine(
         # In-memory sqlite database cannot be accessed from different
         # threads, use file.
-        f'sqlite://{expanduser(Configs.cfg.db.path)}', strategy=ASYNCIO_STRATEGY)
+        f'{expanduser(Configs.cfg.db.url)}')
     conn = await engine.connect()  # Insert some users
---
+++ ticket_man/tables/todo.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+from ticket_man.tables import Base
+
+
+class Todo(Base):
+    __tablename__ = 'todo'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
