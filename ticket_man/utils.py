c9f6ac231eff796aaf277fa212fc64f6a408f6b8
IotaSpencer <me@iotaspencer.me>
2022-08-28 23:56:28 -0400

    utils.py: add dotdict method

---
+++ ticket_man/utils.py
@@ -0,0 +1,13 @@
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, d):
+        super().__init__()
+        for k, v in d.items():
+            if isinstance(v, dict):
+                self[k] = dotdict(v)
+            else:
+                self[k] = v
