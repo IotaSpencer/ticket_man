312d8be4135f839fe92593daf2e0928f21cc89ec
IotaSpencer <me@iotaspencer.me>
2022-08-28 23:41:09 -0400

    fix(): the db works now, it helps if its facing the internet

--- ticket_man/db.py
+++ ticket_man/db.py
@@ -1,8 +1,6 @@
-from os.path import expanduser
 import asyncio
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.future import select
 from sqlalchemy.orm import declarative_base
@@ -11,14 +9,11 @@ from sqlalchemy.orm import selectinload
 from sqlalchemy.orm import sessionmaker
 
 from ticket_man.config import Configs
-from ticket_man.tables import *
+from ticket_man.tables.todo import *
 
 dbcfg = Configs.cfg.db
+print(f'{dbcfg.proto}://{dbcfg.user}:{dbcfg.password}@{dbcfg.host}/{dbcfg.database}?charset=utf8mb4')
+db_engine = create_async_engine(
+    f'{dbcfg.proto}://{dbcfg.user}:{dbcfg.password}@{dbcfg.host}/{dbcfg.database}?charset=utf8mb4&read_timeout=30&write_timeout=30&connect_timeout=30')
 
-async def db():
-    """
-    :rtype: Connection
-    """
-    engine = create_async_engine(f'{dbcfg.proto}://{dbcfg.user}:{dbcfg.password}@{dbcfg.host}/{dbcfg.database}?charset=utf8mb4')
-    conn = await engine.connect()  # type: Connection
-    return conn
+async_session = sessionmaker(db_engine, expire_on_commit=False, class_=AsyncSession)
--- ticket_man/tables/__init__.py
+++ ticket_man/tables/__init__.py
@@ -1,3 +1,4 @@
+__all__ = ['todo', 'Base']
 from sqlalchemy.orm import declarative_base
 from . import todo
 # declarative base class
