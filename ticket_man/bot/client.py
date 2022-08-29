3292e7e308dfcb9881dd880d0a52f41533ff7c91
IotaSpencer <me@iotaspencer.me>
2022-08-28 23:42:17 -0400

    debug(): don't 'store' the extensions

--- ticket_man/bot/client.py
+++ ticket_man/bot/client.py
@@ -26,14 +26,14 @@ async def start(env: str) -> None:
         bot = Bot(intents=discord.Intents.all())
         token = Configs.cfg.bot.token
         # bot.disable_sending = True
-        bot.load_extension('jishaku')
+        #bot.load_extension('jishaku', store=False)
         bot.load_extension('ticket_man.bot.cogs.devbotcog')
 
     # Load Jishaku
-    bot.load_extension('ticket_man.bot.cogs.admin')
-    bot.load_extension('ticket_man.bot.cogs.extensions')
-    bot.load_extension('ticket_man.bot.cogs.todo')
-    bot.load_extension('ticket_man.bot.cogs.join_message')
-    bot.load_extension('ticket_man.bot.cogs.owner')
+    bot.load_extension('ticket_man.bot.cogs.admin', store=False)
+    bot.load_extension('ticket_man.bot.cogs.extensions', store=False)
+    bot.load_extension('ticket_man.bot.cogs.todo', store=False)
+    bot.load_extension('ticket_man.bot.cogs.join_message', store=False)
+    bot.load_extension('ticket_man.bot.cogs.owner', store=False)
 
     await bot.start(token)
