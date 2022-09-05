57d75a397d4a52461061b6fe77a38454af4471fe
IotaSpencer <me@iotaspencer.me>
2022-09-04 23:28:29 -0400

    feature(cogs/tickets): actually implement submitting tickets
    * They don't go to the database yet

--- ticket_man/bot/cogs/tickets.py
+++ ticket_man/bot/cogs/tickets.py
@@ -1,10 +1,12 @@
 # built-in
-
+from discord import ApplicationContext
 # 3rd party
 from discord.ext.commands import Cog, command
 from discord.commands import slash_command
 # local
 from ticket_man.loggers import logger
+from ticket_man.bot.helpers.modals import MyModal, TicketSubmitView
+from ticket_man.bot.helpers.db_abbrevs import get_ticket_type
 
 
 class Tickets(Cog, command_attrs=dict(hidden=True)):
@@ -13,10 +15,9 @@ class Tickets(Cog, command_attrs=dict(hidden=True)):
         self.ext_path = 'ticket_man.bot.cogs.tickets'
 
     @slash_command(name="ticket")
-    async def ticket(self, ctx):
-        pass
-
-
+    async def ticket(self, ctx: ApplicationContext):
+        await ctx.defer(ephemeral=True)
+        await ctx.respond(view=TicketSubmitView())
 
 
 def setup(bot):
