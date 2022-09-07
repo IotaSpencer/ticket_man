583311919472a62ccab8a2b739ffe9f668c9ff46
IotaSpencer <me@iotaspencer.me>
2022-09-07 00:54:08 -0400

    refactor(cogs/tickets.py): import some stuff plus other stuff
    * import sqlalchemy.orm.relationship, sqlalchemy.DateTime
    * make a ticket comments table and insert relationships

--- ticket_man/tables/tickets.py
+++ ticket_man/tables/tickets.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from ticket_man.tables.todo import Base
 
@@ -10,7 +11,7 @@ class Tickets(Base):
     subject = Column(String)
     content = Column(String)
     type = Column(Integer), ForeignKey('ticket_types.id')  # 1 - bug, 2 - feature, 3 - support
-
+    comments = relationship('Comments', back_populates='ticket_comments')
 
 class TicketTypes(Base):
     __tablename__ = 'ticket_types'
@@ -18,3 +19,14 @@ class TicketTypes(Base):
     id = Column(Integer, primary_key=True)
     desc = Column(String)
     type_name = Column(String)
+
+
+class TicketComments(Base):
+    __tablename__ = 'ticket_comments'
+
+    id = Column(Integer, primary_key=True)
+    ticket_id = Column(Integer), ForeignKey('tickets.id')
+    datetime = Column(DateTime)
+    content = Column(String)
+
+    ticket = relationship('Ticket', back_populates='comments')
