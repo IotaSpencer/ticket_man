3be2cef563c4f1046be2bafd0f66a3375b96a476
IotaSpencer <me@iotaspencer.me>
2022-10-19 05:00:07 -0400

    refactor: change how is_server_owner works

--- ticket_man/bot/helpers/__init__.py
+++ ticket_man/bot/helpers/__init__.py
@@ -3,10 +3,16 @@
 # 3rd party
 from typing import Union
 
+import tribool as tribool
 from discord import ApplicationContext
 from discord.ext.commands import Converter, Context
+
+
 # local
 
 
-async def is_server_owner(ctx: Union[Context, ApplicationContext]):
-    return ctx.author.id == ctx.guild.owner_id
+async def is_server_owner(ctx: Union[Context, ApplicationContext]) -> bool | None:
+    if ctx.guild:
+        return ctx.author == ctx.guild.owner
+    else:
+        return None
