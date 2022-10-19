246e1caf5a2a058eae38e53c7ed05181bce6c1ef
IotaSpencer <me@iotaspencer.me>
2022-10-19 05:00:44 -0400

    refactor: make db.py session expire_on_commit=True

--- ticket_man/db.py
+++ ticket_man/db.py
@@ -16,4 +16,4 @@ dbcfg = Configs.cfg.db
 db_engine = create_engine(
         f'{dbcfg.proto}://{dbcfg.user}:{dbcfg.password}@{dbcfg.host}/{dbcfg.database}?charset=utf8mb4&read_timeout=30&write_timeout=30&connect_timeout=30',
         echo=True)
-session = sessionmaker(db_engine, expire_on_commit=False, future=True)
+session = sessionmaker(db_engine, expire_on_commit=True, future=True)
