8a30c66b83b0c22d69d8217a16a7d3f570a8ddad
IotaSpencer <me@iotaspencer.me>
2022-11-04 06:09:34 -0400

    refactor: delete unused embeds

--- ticket_man/bot/helpers/ticket_objects/embeds/ticket_list.py
+++
@@ -1,5 +0,0 @@
-from ticket_man.bot.helpers.ticket_objects.embeds import EmbedBase
-
-
-class ListTicketEmbed(EmbedBase):
-    pass
--- ticket_man/bot/helpers/ticket_objects/embeds/ticket_view.py
+++
@@ -1,8 +0,0 @@
-import discord
-from ticket_man.bot.helpers.ticket_objects.base_embed import EmbedBase
-
-
-class ViewTicketEmbed(EmbedBase):
-    def __init__(self, ticket: discord.Message):
-        super().__init__()
-        self.ticket = ticket
